0e0a319435195f97c7d8c982c3e1d5fc72e00fbd
JULIAN RUEDA <JULIAN RUEDA@DESKTOP-CSKGL2R>
2018-10-15 17:06:35 -0500

    se agregan las interfaces

---
+++ acw/src/main/java/colegio/josefelixrestrepo/acw/main/ColegioJosefelix.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colegiojosefelix;
+
+/**
+ *
+ * @author JULIAN RUEDA
+ */
+public class ColegioJosefelix {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
